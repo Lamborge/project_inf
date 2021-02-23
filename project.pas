d7b90ec48166fae769af067919f822de41f66b1b
Lamborge <maxpopelov@gmail.com>
2021-02-23 12:59:13 +0300

    added base functional

--- project.pas
+++ project.pas
@@ -7,19 +7,29 @@ function sum_arr(z:array of integer):integer;
 var sum, _i:integer;
 begin
   sum:=0;
-    for _i:=0 to length(z) do sum:=sum+z[_i];
-    sum_arr:=sum;
+  for _i:=0 to length(z) do sum:=sum+z[_i];
+  sum_arr:=sum;
 end;
 
-funcion max_el(z:array of integer):integer;
+function max_el(z:array of integer):integer;
 var _i:byte; max:integer;
 begin
   max:=0;
-  for _i:=0 to length(z) do
-    begin
-      if z[_i]>max then max:=z[_i];
-    end;
+  for _i:=0 to length(z) do if z[_i]>max then max:=z[_i];
+  max_el:=max;
 end;
+
+function max_num(x,y:integer):integer;
 begin
+  if x>y then max_num:=x else max_num:=y;
+end;
+
+begin
+  writeln('Введите кол-во баранов в стаде 1: '); read(N);
+  writeln('Введите кол-во баранов в стаде 2: '); read(K);
+  writeln('Введите кол-во баранов в стаде 3: '); read(M);
+  SetLength(A,N);
+  SetLength(B,K);
+  SetLength(C,M);
   
 end.
